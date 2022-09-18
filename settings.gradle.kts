90dfbedbc1436502d8f443ad430a75954a5b945b
Tino <tino@hertlein.dev>
2022-09-18 13:55:27 +0200

    chore: create folders for subprojects

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+//    repositories.gradlePluginPortal()
+//    repositories.google()
+//
+//    repositories.maven(("https://...")) {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+//
+//    includeBuild("gradle/plugins")
+}
+
+
+dependencyResolutionManagement {
+    // repositories.mavenCentral()
+    // repositories.google()
+    // repositories.maven(("https://...")) {
+    // credentials.username = "..."
+    // credentials.password = "..."
+    // }
+//
+    // includeBuild("../my-other-project")
+}
+
+rootProject.name = "my-project"
+
+include("app")
+include("business-logic")
+include("data-model")
